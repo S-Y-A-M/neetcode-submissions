af2a9d4c8f46fe5fe2555516e3fe551d8a57577a
S-Y-A-M <80771420+S-Y-A-M@users.noreply.github.com>
2026-07-29 15:29:12 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    
+    reverseString(s) {
+
+        let rev = s.reverse()
+          return rev
+    }
+}
+const solution = new Solution()
+console.log(solution.reverseString(["n","e","e","t"]))
