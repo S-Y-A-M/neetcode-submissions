a4a8c3eb848d0b3f60fa08d44c48aae84b076180
S-Y-A-M <80771420+S-Y-A-M@users.noreply.github.com>
2026-08-29 11:08:32 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+
+        if ( s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            stack.push(s[i]);
+        } else {
+
+           if (stack.length === 0) {
+                return false;
+            }
+
+            let last = stack.pop();
+
+            if (
+                (s[i] === ')' && last !== '(') ||
+                (s[i] === ']' && last !== '[') ||
+                (s[i] === '}' && last !== '{')
+            ) {
+                return false;
+            }
+        }
+    }
+
+    
+    return stack.length === 0;
+    }
+}
+let solution = new Solution();
+console.log(solution.isValid("([{}])"))
